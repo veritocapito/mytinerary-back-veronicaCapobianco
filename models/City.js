c57da15516675ff9047e58a607a5f706528cd573
Veronica Capobianco <veritocapito@gmail.com>
2023-08-17 19:46:44 -0300

    City model updated

--- models/City.js
+++ models/City.js
@@ -6,6 +6,7 @@ const schema = new Schema({
     city: {type:String, required:true},
     country: {type:String, required:true},
     image: {type:String, required:true},
+    description: {type:String, required:true},
     created_by_user: {type:Types.ObjectId, ref:'users'},
 },{
     timestamps: true,
